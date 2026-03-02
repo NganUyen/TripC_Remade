78ba8001016fac993c2792a2b563107de6c084cf
NganUyen <nguyenlekhanhan2k5@gmail.com>
2026-03-02 21:12:08 +0700

    style: Format import statements and component return for consistency

--- Project/app/partner/hotel/page.tsx
+++ Project/app/partner/hotel/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { HotelPortal } from '@/components/partner/hotel/HotelPortal'
-import { PartnerAuthGuard } from '@/components/partner/PartnerAuthGuard'
+import { HotelPortal } from "@/components/partner/hotel/HotelPortal";
+import { PartnerAuthGuard } from "@/components/partner/PartnerAuthGuard";
 
 export default function HotelPortalPage() {
-    return (
-        <PartnerAuthGuard portalName="Hotel Portal">
-            <HotelPortal />
-        </PartnerAuthGuard>
-    )
+  return (
+    <PartnerAuthGuard portalName="Hotel Portal">
+      <HotelPortal />
+    </PartnerAuthGuard>
+  );
 }
